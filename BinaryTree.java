47a4d9de3b613cf36cc4fbbc65aa00422779fee4
xyuzuki <lindaliu@dyn-169-226-221-228.wireless.albany.edu>
2017-05-03 10:36:52 -0400

    Construct
    Constructors

--- BinaryTree.java
+++ BinaryTree.java
@@ -3,5 +3,21 @@ import java.io.*;
 
 
 public class BinaryTree {
- 
- }
+  
+  static Node r;
+  static int in = 0;
+  
+  class Node
+  {
+    String value;
+    Node left;
+    Node right;
+    
+    Node(String val)
+    {
+      value = val;
+      left = null;
+      right = null;
+    }
+  }
+}
